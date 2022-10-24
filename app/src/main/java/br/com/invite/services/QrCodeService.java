41835a8dd2762d3e8b3e838d6c64e853e910ff6c
Wesley Claudino <claudinowesley11@gmail.com>
2022-10-24 10:57:27 -0300

    :twisted_rightwards_arrows: Mesclando branch "wesley"

---
+++ app/src/main/java/br/com/invite/services/QrCodeService.java
@@ -0,0 +1,33 @@
+package br.com.invite.services;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class QrCodeService extends Service {
+    private final String dominio = "https://chart.googleapis.com/chart?";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void mostrarQrCode(Context context, ImageView imagemView) {
+        Glide.with(context).load(gerarQrCode("teste", 400)).into(imagemView);
+    }
+
+    public String gerarQrCode(String data, Integer tamanho) {
+        tamanho = (tamanho == null ? 200 : tamanho);
+
+        String dimensoes = "chs=" + tamanho + "x" + tamanho;
+        String tipo = "&cht=qr";
+        String texto = "&chl=" + data;
+
+        return dominio + dimensoes + tipo + texto;
+    }
+}
--- app/src/main/java/br/com/invite/view/GerarConviteActivity.java
+++ app/src/main/java/br/com/invite/view/GerarConviteActivity.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.invite.R;
+import br.com.invite.services.QrCodeService;
 
 public class GerarConviteActivity extends AppCompatActivity {
+    private final QrCodeService _qrCodeService = new QrCodeService();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerar_convite);
+
+        // Só pra testar se o qr-code está sendo gerado, excluir depois
+        _qrCodeService.mostrarQrCode(this, findViewById(R.id.iv_qrCode));
+
+        // Gerar o qr-code com os dados do CONVITE
+//        _qrCodeService.gerarQrCode(new Convite(new Evento(), new Usuario()).getSerialId().toString(), null);
     }
 }
