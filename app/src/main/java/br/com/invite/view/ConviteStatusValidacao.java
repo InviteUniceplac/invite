cfcd1ff30d4bb612fec22cc1c29350d9db9985e9
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-05-08 17:02:25 -0300

    :bento: add assets

---
+++ app/src/main/java/br/com/invite/view/ConviteStatusValidacao.java
@@ -0,0 +1,16 @@
+package br.com.invite.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.invite.R;
+
+public class ConviteStatusValidacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convite_status_validacao);
+    }
+}
