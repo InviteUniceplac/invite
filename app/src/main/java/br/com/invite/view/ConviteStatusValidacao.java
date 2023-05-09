efe95616d06fca8cfead39da2155589d804dcfa5
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-05-08 23:57:45 -0300

    :sparkles: add ConviteStatusValidacao activity

--- app/src/main/java/br/com/invite/view/ConviteStatusValidacao.java
+++ app/src/main/java/br/com/invite/view/ConviteStatusValidacao.java
@@ -2,15 +2,35 @@ package br.com.invite.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import br.com.invite.R;
 
 public class ConviteStatusValidacao extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convite_status_validacao);
+
+        TextView messagemStatusValidacao = findViewById(R.id.messagemValidacao);
+        ImageView imagemStatusValidacao = findViewById(R.id.imagemStatusValidacao);
+
+//        Bundle data = getIntent().getExtras();
+        int httpStatus = 200;
+        String httpMessage = "Convite validado com sucesso!";
+
+        if (httpStatus == 400) {
+            messagemStatusValidacao.setTextColor(getResources().getColor(R.color.red_failed));
+            imagemStatusValidacao.setImageResource(R.drawable.error);
+        } else if (httpStatus == 503) {
+            messagemStatusValidacao.setTextColor(getResources().getColor(R.color.black));
+            imagemStatusValidacao.setImageResource(R.drawable.warning);
+        }
+        messagemStatusValidacao.setText(httpMessage);
     }
 }
